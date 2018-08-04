2da3004dc7377bb752645182f5982d30d3b2b424
anthinkingcoder <zhoul@3vjia.com>
2018-08-05 00:33:18 +0800

    add response interceptor

--- src/fetch.js
+++ src/fetch.js
@@ -17,10 +17,13 @@ coreHeaders = Headers
 
 
 const zyFetch = async function (url, option) {
-  const chain = []
+  const chain = [coreFetch]
   let request = new coreRequest(url, option)
   let promise = Promise.resolve(request)
   zyFetch.interceptors.request.forEach(interceptor => {
+    chain.unshift(interceptor.resolve, interceptor.reject)
+  })
+  zyFetch.interceptors.response.forEach(interceptor => {
     chain.push(interceptor.resolve, interceptor.reject)
   })
 
@@ -28,12 +31,13 @@ const zyFetch = async function (url, option) {
     promise = promise.then(chain.shift(), chain.shift())
   }
   //wait request chain finished
-  await promise.then(() => {
-    // console.info('chain处理成功')
-  }).catch(() => {
-    // console.info('chain处理失败')
-  })
-  return coreFetch(request)
+  // await promise.then(() => {
+  // console.info('chain处理成功')
+  // }).catch(() => {
+  // console.info('chain处理失败')
+  // })
+
+  return promise
 
 }
 
