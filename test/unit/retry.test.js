f780813339c1501a0bb816af0571dff9525536ff
anthinkingcoder <jojo837769723@gmail.com>
2018-08-26 13:08:45 +0800

    retry test

---
+++ test/unit/retry.test.js
@@ -0,0 +1,14 @@
+import fetch from '../../lib/zyFetchs'
+fetch.config.baseUrl = 'https://www.easy-mock.com/mock/5b69ab3df770e33d7c13428e/zy-fetch'
+describe('testing retry count', () => {
+  test('test retry', () => {
+    return fetch.get(`/retry`,{
+      retry:3,
+      timeout:10
+    }).then(function (response) {
+      return response.json()
+    }).catch(function (error) {
+      expect(error).toEqual(new Error('Network request timeout'))
+    })
+  })
+});
